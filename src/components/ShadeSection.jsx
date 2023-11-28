0180c6d895c498ef657d41446f34f7df72885466
reinexxism <lucy0805@naver.com>
2023-11-28 13:48:58 +0900

    ADD : Light & Dark Theme button

--- src/components/ShadeSection.jsx
+++ src/components/ShadeSection.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./ShadeSection.css";
 import ColorChip from "./ColorChip";
+import Theme from "./Theme";
 
 export default function ShadeSection() {
   return (
     <div className="shade-section-container scroll">
       <div className="title">
         <h3>Shades of GREEN</h3>
+        <Theme />
       </div>
       <div className="shade-layer">
         <ColorChip name="Frosted Jade" />
---
+++ src/components/Theme.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Theme.css";
+
+// Light Theme & Dark Theme
+export default function Theme() {
+  return (
+    <div className="theme-container">
+      <button>BTN</button>
+    </div>
+  );
+}
