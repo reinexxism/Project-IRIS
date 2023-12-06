6a410d6aa335b515abf43a4eaf63a64937ee4223
reinexxism <lucy0805@naver.com>
2023-12-06 16:22:15 +0900

    MOD: JSON 값으로 bg 변경

--- src/components/ColorChip.jsx
+++ src/components/ColorChip.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./ColorChip.css";
 
-export default function ColorChip({ name }) {
+export default function ColorChip({ name, color }) {
   return (
-    <div className="color-chip-container">
+    <div className="color-chip-container" style={{ backgroundColor: color }}>
       <span className="color-chip-name">{name}</span>
     </div>
   );
